7bbb49551cca9fc54b29c07d21f0787eea61ba32
rdesfonds-campus <romain.desfonds@le-campus-numerique.fr>
2026-05-05 23:33:52 +0200

    feat: créer composant AppNavbar (#1)

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <div class="nav-links">
+      <RouterLink to="/">Films</RouterLink>
+      <RouterLink to="/">Collections</RouterLink>
+      <RouterLink to="/">À propos</RouterLink>
+    </div>
+    <RouterLink to="/login">
+      <button>Connexion</button>
+    </RouterLink>
+  </nav>
+</template>
+
+<script setup>
+</script>
--- src/views/MovieListing.vue
+++ src/views/MovieListing.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
+    <AppNavbar />
     <h1>Page Listing Movies 🎬</h1>
   </div>
 </template>
 
 <script setup>
+import AppNavbar from '@/components/AppNavbar.vue'
 </script>
-
-<style scoped>
-</style>
